27c5a89bdf6d4a18cc345575ee6663328409f171
Ayoub Zekhnine <sawtiayoub@hotmail.com>
2026-04-25 18:16:12 +0200

    metting

--- src/components/CalendlySection.tsx
+++ src/components/CalendlySection.tsx
@@ -33,7 +33,7 @@ function CalendlySection() {
 
         <div className="rounded-2xl overflow-hidden bg-white">
           <iframe
-            src="https://calendly.com/vexolead/new-meeting?embed_domain=www.vexolead.com&embed_type=Inline"
+            src="https://calendly.com/vexolead/reunion-45-min"
             width="100%"
             height="700"
             frameBorder="0"
